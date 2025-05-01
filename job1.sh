ac5ede6d7eb0064079501d72fee3983acec58e24
Gosia-Kwap <malgosiakwap@gmail.com>
2025-05-01 18:13:17 +0200

    first job script

---
+++ job1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH --time=04:00:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --job-name=first_attempt
+#SBATCH --mem=10GB
+
+# Create venv if not exists
+if [ ! -d "thesis_venv" ]; then
+  python3 -m venv thesis_venv
+fi
+
+# Activate venv
+source venv/bin/activate
+
+module purge
+
+module load Python/3.9.6-GCCcore-11.2.0
+
+source ~/env/bin/activate
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
+python ./src/main.py
+
+deactivate
