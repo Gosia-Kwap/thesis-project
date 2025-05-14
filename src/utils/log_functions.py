014276b62f50aad05f522137946ee40d1f57bac3
Gosia-Kwap <malgosiakwap@gmail.com>
2025-05-14 14:11:17 +0200

    Adding default info level to log messages

--- src/utils/log_functions.py
+++ src/utils/log_functions.py
@@ -1,3 +1,5 @@
-def log_message(message, level):
+from src.utils.Enums import LEVEL
+
+def log_message(message, level = LEVEL.INFO):
     """Helper function for consistent logging format"""
     print(f"[{level}] {message}")
