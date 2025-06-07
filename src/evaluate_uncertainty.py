4e802a056d0dcbb1c65579686c910d9bfd020c2d
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-06-07 23:30:00 +0200

    eval job fix

--- src/evaluate_uncertainty.py
+++ src/evaluate_uncertainty.py
@@ -85,7 +85,7 @@ def main(executor: str = "habrok", task: str = "SVAMP", model: str = "gemma9b", 
         # Load a specific index
         df = pd.read_json(f"{result_dir}/{task}_perturbed_outputs_{model}_{index}_{index + 100}.json")
         df["uncertainty"] = df.apply(lambda row: compute_uncertainty_for_row(row, method), axis=1)
-        output_dir = f"{result_dir}/uncertainty/{task}_perturbed_outputs_{model}_{index}_uncertainty.json"
+        output_dir = f"{result_dir}/uncertainty/{task}_perturbed_outputs_{model}_{index}_uncertainty_{method}.json"
         df.to_json(output_dir, orient="records")
         log_message(f"Finished execution with parameters: index={index}, task={task}, model={model}")
         log_message(f"Results saved to {output_dir}")
@@ -93,8 +93,8 @@ def main(executor: str = "habrok", task: str = "SVAMP", model: str = "gemma9b", 
         dataframes = [pd.read_json(f"{result_dir}/{task}_perturbed_outputs_{model}_{i}_{i + 100}.json") for i in
                       range(0, 1000, 100)]
         combined_df = pd.concat(dataframes, ignore_index=True)
-        combined_df["uncertainty"] = df.apply(lambda row: compute_uncertainty_for_row(row, method), axis=1)
-        output_dir = f"{result_dir}/{task}_perturbed_outputs_{model}_uncertainty.json"
+        combined_df["uncertainty"] = combined_df.apply(lambda row: compute_uncertainty_for_row(row, method), axis=1)
+        output_dir = f"{result_dir}/{task}_perturbed_outputs_{model}_uncertainty_{method}.json"
         combined_df.to_json(output_dir, orient="records")
         log_message(f"Finished execution with parameters: task={task}, model={model}")
         log_message(f"Results saved to {output_dir}")
