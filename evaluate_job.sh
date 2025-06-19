65f2df28b02dbd5fa81add87a98e718109fc7b2e
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-06-19 21:26:38 +0200

    llama gsm8k entail

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks=1
 #SBATCH --array=0-13
-#SBATCH --job-name=evaluate_cos_llama_gsm8
+#SBATCH --job-name=evaluate_entailment_llama_gsm8k
 #SBATCH --mem=10GB
 
 module purge
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model llama3  --index ${START_INDEX} --method cosine --task SVAMP
+python -m src.evaluate_uncertainty --model llama3  --index ${START_INDEX} --method entailment_prob --task GSM8K
 
 deactivate
