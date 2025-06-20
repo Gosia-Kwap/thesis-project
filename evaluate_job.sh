a448be32a549b381f0c556f55cf054ffc56873b7
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-06-20 23:36:13 +0200

    job

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=04:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks=1
-#SBATCH --array=0-13
-#SBATCH --job-name=evaluate_gsm8k_llama_entail
+#SBATCH --array=1-13
+#SBATCH --job-name=evaluate_llama_ai2_arc_cos
 #SBATCH --mem=10GB
 
 module purge
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model llama3  --index ${START_INDEX} --method entailment --task GSM8K
+python -m src.evaluate_uncertainty --model llama3  --index ${START_INDEX} --method cos --task ai2_arc
 
 deactivate
