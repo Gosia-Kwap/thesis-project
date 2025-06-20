c89a3d5bf88af1090ac53499607c363cf24d7c8e
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-06-20 14:10:52 +0200

    adiv job

--- job3.sh
+++ job3.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --array=0-15
-#SBATCH --job-name=asdiv-gemma
+#SBATCH --job-name=asdiv-llama
 #SBATCH --mem=10GB
 
 module purge
@@ -38,6 +38,6 @@ END_INDEX=$(((SLURM_ARRAY_TASK_ID + 1) * ROWS_PER_TASK))
 export HF_HOME=/tmp
 
 # Run the script with args
-python -m src.main --model gemma9b --start ${START_INDEX} --end ${END_INDEX} --task ASDiv
+python -m src.main --model llama3 --start ${START_INDEX} --end ${END_INDEX} --task ASDiv
 
 deactivate
