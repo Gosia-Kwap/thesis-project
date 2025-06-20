0815d427a3a17f6d2412f14e522ca77c5f80ef9a
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-06-20 08:39:55 +0200

    job

--- job3.sh
+++ job3.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --array=0-15
-#SBATCH --job-name=cqa-gemma
+#SBATCH --job-name=cqa-llama
 #SBATCH --mem=10GB
 
 module purge
@@ -38,6 +38,6 @@ END_INDEX=$(((SLURM_ARRAY_TASK_ID + 1) * ROWS_PER_TASK))
 export HF_HOME=/tmp
 
 # Run the script with args
-python -m src.main --model gemma9b --start ${START_INDEX} --end ${END_INDEX} --task CommonsenseQA
+python -m src.main --model llama3 --start ${START_INDEX} --end ${END_INDEX} --task CommonsenseQA
 
 deactivate
