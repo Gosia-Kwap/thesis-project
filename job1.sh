33360db497f6a7ca594c1c0334f922bb95d927b2
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-11 15:12:21 +0200

    full first  run try

--- job1.sh
+++ job1.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --array=0
-#SBATCH --job-name=svamp-llama-test
+#SBATCH --job-name=svamp-gemma-test
 #SBATCH --mem=10GB
 
 module purge
@@ -39,6 +39,6 @@ END_INDEX=$(((SLURM_ARRAY_TASK_ID + 1) * ROWS_PER_TASK))
 export HF_HOME=/tmp
 
 # Run the script with args
-python -m src.main --backend llama_cpp --model llama3 --start ${START_INDEX} --end ${END_INDEX} --task SVAMP --quantisation 4
+python -m src.main --backend llama_cpp --model gemma9b --start ${START_INDEX} --end ${END_INDEX} --task SVAMP --quantisation 4
 
 deactivate
