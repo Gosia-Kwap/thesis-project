d0792a6b6af1712f18047a6dbdb3612186244b07
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-11 18:00:41 +0200

    job variant

--- job3.sh
+++ job3.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --array=0-15
-#SBATCH --job-name=cqa-deepseek
+#SBATCH --job-name=cqa-deepseek_4k
 #SBATCH --mem=10GB
 
 module purge
@@ -38,6 +38,6 @@ END_INDEX=$(((SLURM_ARRAY_TASK_ID + 1) * ROWS_PER_TASK))
 export HF_HOME=/tmp
 
 # Run the script with args
-python -m src.main --model deepseek --start ${START_INDEX} --end ${END_INDEX} --task CommonsenseQA
+python -m src.main --model deepseek --backend llama_cpp --start ${START_INDEX} --end ${END_INDEX} --task CommonsenseQA --quantisation 4
 
 deactivate
