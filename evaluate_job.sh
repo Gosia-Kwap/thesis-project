b35cd808bcbb39cb1df9b0d31b85b31ef72f4eb9
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-13 23:06:07 +0200

    eval jobs

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks=1
 #SBATCH --array=1-15
-#SBATCH --job-name=eval_llama_logiqa_quant4
+#SBATCH --job-name=eval_llama_logiqa_quant6
 #SBATCH --mem=10GB
 
 module purge
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model llama3  --index ${START_INDEX} --method cosine --task logiqa --qunatisation 4
+python -m src.evaluate_uncertainty --model llama3  --index ${START_INDEX} --method cosine --task logiqa --qunatisation 6
 
 deactivate
