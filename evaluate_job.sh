532a6806df6465d9f43395f7998ef1adc368e05c
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-13 23:07:50 +0200

    eval jobs

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -5,8 +5,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks=1
-#SBATCH --array=1-15
-#SBATCH --job-name=eval_gemma_logiqa_quant4
+#SBATCH --array=1-9
+#SBATCH --job-name=eval_gemma_svamp_quant4
 #SBATCH --mem=10GB
 
 module purge
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model gemma9b  --index ${START_INDEX} --method cosine --task logiqa --qunatisation 4
+python -m src.evaluate_uncertainty --model gemma9b  --index ${START_INDEX} --method cosine --task svamp --qunatisation 4
 
 deactivate
