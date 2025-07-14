60aa8012eb8b9750667dab076591646ea115d7b1
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-14 12:55:01 +0200

    deepseek jobs

--- job1.sh
+++ job1.sh
@@ -39,6 +39,6 @@ END_INDEX=$(((SLURM_ARRAY_TASK_ID + 1) * ROWS_PER_TASK))
 export HF_HOME=/tmp
 
 # Run the script with args
-python -m src.main --model deepseek --backend llama_cpp --start ${START_INDEX} --end ${END_INDEX} --task SVAMP --quantisation 6
-
+#python -m src.main --model deepseek --backend llama_cpp --start ${START_INDEX} --end ${END_INDEX} --task SVAMP --quantisation 6
+python -m src.main --model deepseek --start ${START_INDEX} --end ${END_INDEX} --task SVAMP
 deactivate
