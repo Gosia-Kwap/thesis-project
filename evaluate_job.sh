9baa6ee8c371815dd76070f328b35f1f4c3a36e1
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-17 17:26:05 +0200

    eval jobs

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=02:00:00
+#SBATCH --time=03:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks=1
-#SBATCH --array=0,2,4,6,8
-#SBATCH --job-name=eval-svamp-deepseek-quant4-part1
+#SBATCH --array=0,2,6,8,10
+#SBATCH --job-name=eval-cqa-llama-quant4-part1
 #SBATCH --mem=10GB
 
 module purge
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model deepseek --index ${START_INDEX} --method cosine --task SVAMP --quantisation 4
+python -m src.evaluate_uncertainty --model llama3 --index ${START_INDEX} --method cosine --task CommonsenseQA --quantisation 4
 
 deactivate
