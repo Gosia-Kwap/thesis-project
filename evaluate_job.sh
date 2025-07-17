53ee6fcca62e235204ae3779b4f2d89504acbea6
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-17 18:24:38 +0200

    eval jobs

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --time=03:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks=1
 #SBATCH --array=0,2,4,6,8
-#SBATCH --job-name=eval-asdiv-deepseek-quant6-part1
+#SBATCH --job-name=eval-redo-ai2_arc_gemma
 #SBATCH --mem=10GB
 
 module purge
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model deepseek --index ${START_INDEX} --method cosine --task ASDiv --quantisation 4
+python -m src.evaluate_uncertainty --model gemma9b --index ${START_INDEX} --method cosine --task ai2_arc
 
 deactivate
