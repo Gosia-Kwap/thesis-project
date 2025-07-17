207926695eff9b71ac9d90d96733b6bca0e6818d
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-17 16:48:46 +0200

    eval jobs

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model llama3 --index ${START_INDEX} --method cosine --task svamp --quantisation 6
+python -m src.evaluate_uncertainty --model llama3 --index ${START_INDEX} --method cosine --task SVAMP --quantisation 6
 
 deactivate
