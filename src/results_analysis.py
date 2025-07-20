e84721762b8c53feb940aec6ebeb3e6112a677e8
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-20 13:41:13 +0200

    Imrperovwe logging for the result analysis runs

--- src/results_analysis.py
+++ src/results_analysis.py
@@ -1,20 +1,30 @@
 from src.uncertainty.UncertaintyCalibrationAnalyser import UncertaintyCalibrationAnalyser
+import os
+import glob
 from src.utils.log_functions import log_message
 
 tasks = ['SVAMP', 'GSM8K', 'ASDiv', 'ai2_arc', 'logiqa', 'CommonsenseQA']
 quantisations = ['', '_4', '_6']
-methods = ['cosine']
+methods = ['cosine', 'entailment', 'entailment_prob']
 models = ['gemma9b', 'llama3', 'deepseek']
-method = methods[0]
+result_dir = f"/Users/University/Library/CloudStorage/OneDrive-Personal/Dokumenty/studia/AI/Year3/ThesisAI/thesis-project/results/uncertainty/"
+
+def files_exist(task, model, method, quantisation, result_dir):
+    pattern = os.path.join(
+        result_dir,
+        f"{task}_perturbed_outputs_{model}_*_uncertainty_{method}{quantisation}.json"
+    )
+    return len(glob.glob(pattern)) > 0
 
 def analyse_results(task, model, method, quantisation):
-    result_dir = f"/Users/University/Library/CloudStorage/OneDrive-Personal/Dokumenty/studia/AI/Year3/ThesisAI/thesis-project/results/uncertainty/"
+
     plot_dir = f"/Users/University/Library/CloudStorage/OneDrive-Personal/Dokumenty/studia/AI/Year3/ThesisAI/thesis-project/plots/{task}/{task}_{method}_{model}{quantisation}"
     stats_dir = f"/Users/University/Library/CloudStorage/OneDrive-Personal/Dokumenty/studia/AI/Year3/ThesisAI/thesis-project/results/stats/{task}/{model}_{method}{quantisation}"
 
 
 
     # Initialize the analyser with the directory containing results
+
     analyser = UncertaintyCalibrationAnalyser(
         result_dir=result_dir,
         file_pattern=f"{task}_perturbed_outputs_{model}_*_uncertainty_{method}{quantisation}.json",
@@ -26,7 +36,7 @@ def analyse_results(task, model, method, quantisation):
     analysis_results = analyser.analyze_calibration_conf_unc(save_dir=stats_dir)
     #
     # # Plot calibration comparison
-    analyser.plot_calibration_unc_conf(analysis_results, plot_dir, mode='uncertainty')
+    analyser.plot_calibration_unc_conf(analysis_results, plot_dir)
 
     # Compare uncertainty distributions across types and perturbations
     uncertainty_distributions = analyser.generate_distribution_comparison_per_uncertainty(plot_dir=plot_dir, stats_dir=stats_dir)
@@ -50,14 +60,18 @@ def analyse_results(task, model, method, quantisation):
     # results_df = pd.DataFrame(results)
     analyser.save_calibration_conf_unc(results, output_path=stats_dir+"_overall_stats.json")
 
-for tasks in tasks:
+
+for task in tasks:
     for model in models:
         for quantisation in quantisations:
-            try:
-                log_message(f"Analysis for: {tasks}/{model}{quantisation}/{method}")
-                analyse_results(tasks, model, method, quantisation)
-            except:
-                print(f"Error in analysing {tasks}/{model}{quantisation}/{method}")
-                pass
+            for method in methods:
+                if files_exist(task, model, method, quantisation, result_dir):
+                    try:
+                        log_message(f"Analysis for: {task}/{model}{quantisation}/{method}")
+                        analyse_results(task, model, method, quantisation)
+                    except Exception as e:
+                        log_message(f"Error in {task}-{model}{quantisation}-{method}: {str(e)}")
+                else:
+                    log_message(f"Skipping missing combo {task}-{model}{quantisation}-{method}", level="INFO")
 
 
