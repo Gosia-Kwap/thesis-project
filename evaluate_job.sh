e5736f9a7f3a8153829a68aeaf57d1163799d5f1
Gosia Kwapniewska <malgosiakwap@gmail.com>
2025-07-20 13:03:14 +0200

    adding raw data to the set

--- evaluate_job.sh
+++ evaluate_job.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks=1
 #SBATCH --array=0-9
-#SBATCH --job-name=eval-deepseek-svamp-cosine
+#SBATCH --job-name=eval-deepseek-GSM8K-cosine
 #SBATCH --mem=10GB
 
 module purge
@@ -32,6 +32,6 @@ START_INDEX=$((SLURM_ARRAY_TASK_ID * ROWS_PER_TASK))
 
 export HF_HOME=/tmp
 # Run the script with args
-python -m src.evaluate_uncertainty --model deepseek --index ${START_INDEX} --method cosine --task SVAMP
+python -m src.evaluate_uncertainty --model deepseek --index ${START_INDEX} --method cosine --task GSM8K
 
 deactivate
